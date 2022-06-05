2fcb4b247b44a0a95a210450e32ee1956203bcfb
Ramakrishna Joshi <rjoshi@falabella.cl>
2022-06-05 22:34:30 +0530

    Setup Detail and Favourite Fragments

--- app/src/main/java/com/example/nasaapod/ui/fragments/DetailFragment.kt
+++ app/src/main/java/com/example/nasaapod/ui/fragments/DetailFragment.kt
@@ -5,15 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.example.nasaapod.R
+import com.example.nasaapod.databinding.FragmentDetailBinding
+import com.example.nasaapod.di.factory.ViewModelFactory
+import com.example.nasaapod.ui.viewmodel.HomeViewModel
+import javax.inject.Inject
 
 class DetailFragment : Fragment() {
+
+    @Inject
+    lateinit var viewModelFactory: ViewModelFactory
+
+    val viewModel by viewModels<HomeViewModel>({this.requireActivity()}) {
+        viewModelFactory
+    }
+
+    private var _binding: FragmentDetailBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+        _binding = FragmentDetailBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
--- app/src/main/java/com/example/nasaapod/ui/fragments/FavouriteFragment.kt
+++ app/src/main/java/com/example/nasaapod/ui/fragments/FavouriteFragment.kt
@@ -5,16 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.nasaapod.R
+import androidx.fragment.app.viewModels
+import com.example.nasaapod.databinding.FragmentFavouriteBinding
+import com.example.nasaapod.di.factory.ViewModelFactory
+import com.example.nasaapod.ui.viewmodel.HomeViewModel
+import javax.inject.Inject
 
 class FavouriteFragment : Fragment() {
+
+    @Inject
+    lateinit var viewModelFactory: ViewModelFactory
+
+    val viewModel by viewModels<HomeViewModel>({this.requireActivity()}) {
+        viewModelFactory
+    }
+
+    private var _binding: FragmentFavouriteBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favourite, container, false)
+        _binding = FragmentFavouriteBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
